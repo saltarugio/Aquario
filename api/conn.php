7f84e133b3be1b83c039117d1aeddcad39245802
Victor Silva Altarugio <134739051+saltarugio@users.noreply.github.com>
2024-03-07 14:24:43 -0300

    Update conn.php

--- api/conn.php
+++ api/conn.php
@@ -2,7 +2,7 @@
 $username = "root";
 $password = "root";
 $database = "aquario";
-$host = "localhost";
+$host = "192.168.0.137";
 $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_errno) {
     die("Conexão falhou: (". $conn->connect_errno . ")" . $conn->connect_error); 
